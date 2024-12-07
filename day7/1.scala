cd45ed7b6fb3a12b40a8d9651d9c9d40087b3682
DrThum <thum@drthum.net>
2024-12-07 13:59:05 +0100

    Day 7

---
+++ day7/1.scala
@@ -0,0 +1,31 @@
+import scala.io.Source
+
+case class Equation(result: BigInt, terms: List[BigInt])
+
+@main def main = {
+  val equations = Source.fromFile("input").getLines.map(line => {
+    val Array(resultStr, termsStr) = line.split(": ")
+    val terms = termsStr.split(' ').map(BigInt(_)).toList
+
+    Equation(BigInt(resultStr), terms)
+  }).toList
+
+  val possiblyTrueEquations = equations.filter(checkEquation)
+
+  println(possiblyTrueEquations.map(_.result).sum)
+}
+
+def checkEquation(equation: Equation): Boolean = {
+  checkTerms(equation.result, equation.terms.head, equation.terms.tail, 1)
+}
+
+def checkTerms(expectedResult: BigInt, accumulator: BigInt, restOfTerms: List[BigInt], depth: Int): Boolean = {
+  if (restOfTerms.isEmpty) {
+    expectedResult == accumulator
+  } else {
+    val checkAdd = checkTerms(expectedResult, accumulator + restOfTerms.head, restOfTerms.tail, depth + 1)
+    val checkMultiply = checkTerms(expectedResult, accumulator * restOfTerms.head, restOfTerms.tail, depth + 1)
+
+    checkAdd || checkMultiply
+  }
+}
